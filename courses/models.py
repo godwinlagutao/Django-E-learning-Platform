3bbbff30a3649e1db9b06774c97fa94a406758b5
godwinlagutao <generationsecond2020@gmail.com>
2024-10-14 15:05:58 +0100

    added student registration

--- courses/models.py
+++ courses/models.py
@@ -22,6 +22,7 @@ class Course(models.Model):
     subject = models.ForeignKey(Subject,
                                 related_name='courses',
                                 on_delete=models.CASCADE)
+    students = models.ManyToManyField(User, related_name='courses_joined', blank=True)
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, unique=True)
     overview = models.TextField()
@@ -91,3 +92,4 @@ class Image(ItemBase):
 
 class Video(ItemBase):
     url = models.URLField()
+    
